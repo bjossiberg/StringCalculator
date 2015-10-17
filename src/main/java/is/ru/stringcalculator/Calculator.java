916020e4d5af4a879052ed776c7fbc8a48952dde
bjossiberg <bjornbb13@ru.is>
2015-10-17 04:27:12 +0000

    Step 4 finished string calculator can now handle delimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public static int add(String string){
 		if(string.length() == 0)
 			return 0;
-		return sum(string.split(",|\n"));
+		return sum(values(string));
 	}
 
 	private static int sum(String[] values) {
@@ -18,5 +18,21 @@ public class Calculator {
 
 	private static int toInt(String value) {
 		return Integer.parseInt(value);
+	}
+
+	private static String numbers(String string) {
+		if(string.startsWith("//"))
+			return string.substring(3);
+		return string;
+	}
+
+	private static String delimiter(String string) {
+		if(string.startsWith("//"))
+ 			return string.substring(2,3);
+ 		return ",|\n";
+	}
+
+	private static String[] values(String string) {
+		return numbers(string).split(delimiter(string));
 	}
 }
